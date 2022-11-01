90ca5b6344337e0add343a7d2ca18d3b92f15611
ulukman7 <92907597+ulukman7@users.noreply.github.com>
2022-11-01 11:22:47 +0600

    Add files via upload

---
+++ slovar.py
@@ -0,0 +1,90 @@
+eng1,ru1 = input("Введите Английское слово 1 "),input("Введите перевод 1 ")
+eng2,ru2 = input("Введите Английское слово 2 "),input("Введите перевод 2 ")
+eng3,ru3 = input("Введите Английское слово 3 "),input("Введите перевод 3 ")
+eng4,ru4 = input("Введите Английское слово 4 "),input("Введите перевод 4 ")
+eng5,ru5 = input("Введите Английское слово 5 "),input("Введите перевод 5 ")
+eng6,ru6 = input("Введите Английское слово 6 "),input("Введите перевод 6 ")
+eng7,ru7 = input("Введите Английское слово 7 "),input("Введите перевод 7 ")
+eng8,ru8 = input("Введите Английское слово 8 "),input("Введите перевод 8 ")
+eng9,ru9 = input("Введите Английское слово 9 "),input("Введите перевод 9 ")
+eng10,ru10 = input("Введите Английское слово 10 "),input("Введите перевод 10 ")
+
+print (ru3)
+engl3 = input("напишите перевод:")
+while eng3 != engl3 :  
+    print ("fail")
+    engl3 = input("попробуйте еще раз ")  
+print ("succes")
+
+
+print (ru4)
+engl4 = input("напишите перевод:")
+while eng4 != engl4:  
+    print ("fail")
+    engl4 = input("попробуйте еще раз ")  
+print ("succes")
+
+
+print (ru7)
+engl7 = input("напишите перевод:")
+while eng7 != engl7:  
+    print ("fail")
+    engl7 = input("попробуйте еще раз ")  
+print ("succes")   
+
+
+print (ru9)
+engl9 = input("напишите перевод:")
+while eng9 != engl9:  
+    print ("fail")
+    engl9 = input("попробуйте еще раз ")  
+print ("succes")
+
+
+
+print (ru1)
+engl1 = input("напишите перевод:")
+while eng1 != engl1 :
+    print ("fail")
+    engl1 = input("попробуйте еще раз ")  
+print ("succes")
+
+
+ 
+print (ru5)
+engl5 = input("напишите перевод:")
+while eng5 != engl5:
+    print ("fail")
+    engl5 = input("попробуйте еще раз ")  
+print ("succes")
+
+ 
+print (ru8)
+engl8 = input("напишите перевод:")
+while eng8 != engl8:
+    print ("fail")
+    engl8 = input("попробуйте еще раз ")  
+print ("succes")
+
+ 
+print (eng10)
+engl10 = input("напишите перевод:")
+while eng10 != engl10:
+    print ("fail")
+    engl10 = input("попробуйте еще раз ")  
+print ("succes")
+ 
+print (ru2)
+engl2 = input("напишите перевод:")
+while eng2 != engl2:
+    print ("fail")
+    engl2 = input("попробуйте еще раз ")  
+print ("succes")
+
+ 
+print (ru6)
+engl6 = input("напишите перевод:")
+while eng6 != engl6:
+    print ("fail")
+    engl6 = input("попробуйте еще раз ")  
+print ("succes")
